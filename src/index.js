853c025d936ea06baab33702469f62b7a1136749
Mauricio Rojas <a20171181@pucp.edu.pe>
2023-09-20 20:04:28 -0500

    new look

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import 'react-app-polyfill/ie11'; // For IE 11 support
-import 'react-app-polyfill/stable';
-import 'core-js';
-import './polyfill'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense } from "react";
+// import ReactDOM from "react-dom";
+import {createRoot} from 'react-dom/client';
+import "./assets/scss/style.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
+import Loader from "./layouts/loader/Loader";
 
-import { icons } from './assets/icons'
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-import { Provider } from 'react-redux'
-import store from './store'
+root.render(
+  <Suspense fallback={<Loader />}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Suspense>,
 
-React.icons = icons
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('root')
+  // document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/services/models/SimpleProperty.js
+++
@@ -1,6 +0,0 @@
-// This is a utility class to avoid doing a ton this this.thing = props.thing
-export default class SimpleProperty {
-    constructor (props) {
-      Object.assign(this, props)
-    }
-  }
